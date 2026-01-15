936e4a8a33596943645f08729025a28c3fe1154d
Cristo Dante <cristodante80@gmail.com>
2026-01-15 11:10:10 +0300

    JavaScript giriş yapıldı.

---
+++ 03_javascript_dersleri/01_javascript_temelleri/04_konsol_bildirim_etkilesim/script.js
@@ -0,0 +1,8 @@
+console.log("Bugün günlerden Perşembe")
+const hayvanlar = ["Kedi", "Köpek", "Yılan"]
+console.table(hayvanlar) 
+console.warn("Dikkat Köpek Çıkabilir!!")
+console.error("İşlem gerçekleştirilemedi.!!!")
+
+alert("Sayfaya giriş yaptınız?")
+console.log(prompt("Adınız nedir: "))
